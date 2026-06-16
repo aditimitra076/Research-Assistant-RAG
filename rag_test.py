8dfc838575e3abc4f69162ab84ab3715aa027025
aditimitra076 <aditimitra076@gmail.com>
2026-06-16 17:28:53 +0530

    Added retrieval debugging and improved chunking

--- rag_test.py
+++ rag_test.py
@@ -23,10 +23,10 @@ for page in reader.pages:
 ##############################################################
 #chunking
 
-chunk_size = 1000
+chunk_size = 500
 
 #creating overlapping chunks
-overlap = 200
+overlap = 100
 
 chunks = []
 
@@ -34,7 +34,8 @@ for i in range(0, len(full_text), chunk_size-overlap):
     chunk = full_text[i:i+chunk_size]
     chunks.append(chunk)
 
-
+print("\n===CHUNK 0===\n")
+print(chunks[0])
 
 
 #################################################################
@@ -44,12 +45,14 @@ from sentence_transformers import SentenceTransformer
 
 load_dotenv()
 
-login(token=os.getenv("HF_TOKEN"))
+#login(token=os.getenv("HF_TOKEN"))
 
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
-embeddings = model.encode(chunks)
+embeddings = model.encode(
+    chunks,
+    normalize_embeddings=True)
 
 print("Number of chunks : ", len(chunks))
 print("Number of embeddings: ", len(embeddings))
@@ -66,8 +69,13 @@ print(embeddings.shape)
 
 from sentence_transformers.util import cos_sim
 
+
+#load token
+client = InferenceClient(
+        api_key = os.getenv("HF_TOKEN")
+    )
+
 #user input question
-#query = input("Ask a question: ") #user input of question
 
 while True:
     query = input("\nAsk a question( or type exit): ")
@@ -75,66 +83,93 @@ while True:
     if query.lower()=="exit":
         break
 
+    #query embedding######################################
+    query_embedding = model.encode(
+        query,
+        normalize_embeddings=True)
 
 
-query_embedding = model.encode(query)
-
-scores = []
+    scores = []
 
 
-#stroing chunk embedding values
-for i in range(len(chunks)):
-    score = cos_sim(query_embedding, embeddings[i]).item() #item () converts pytorch tensor object to a normal number for ease of sorting
-    scores.append((score, chunks[i]))
+    #stroing chunk embedding values
+    for i in range(len(chunks)):
+        score = cos_sim(query_embedding, embeddings[i]).item() #item () converts pytorch tensor object to a normal number for ease of sorting
+        scores.append((score,i, chunks[i]))
 
-scores.sort(key=lambda x: x[0], reverse=True)
+    scores.sort(key=lambda x: x[0], reverse=True)
 
-top_3 = scores[:3]
+    #debug retreval
+    print("\nTop 10 scores:\n")
+
+    for score, idx, chunk in scores[:10]:
+        print(f"Chunk{idx} : {score:.4f}")
+
+    top_10 = scores[:10]
+
+    print("\nTop Retrived Chunks: \n")
+
+    for score, idx, chunk in top_10:
+        print(f"\nChunk Index: {idx}")
+        print(f"Score: {score:.4f}")
+        print(chunk[:300])
+
+    #context creation####################################
+    context=""
+
+    for score,idx, chunk in top_10:
+        context+= chunk+ "\n\n"
+
+    # print("\n----CONTEXT----\n")
+    # print(context[:1500])
+
+    
+    #connecting to LLM##########################################
+    
+
+    prompt = f"""
+    You are a helpful reserach assistant.
+
+    Answer the question using ONLY the provided context.
+
+    If the answer is not present in the context, say:
+    "I could not find the answer in the provided document."
+
+    Context: 
+    {context}
+
+    Question:
+    {query}
+
+    """
+
+    ##############################################################3
+    #generating answer
+
+    response = client.chat_completion(
+        model="meta-llama/Llama-3.1-8B-Instruct",
+        messages=[
+            {
+                "role":"user",
+                "content":prompt
+            }
+        ],
+
+        max_tokens=300
+    )
+
+    print("\n====ANSWER====\n")
+    print(response.choices[0].message.content)
 
 
-context=""
-
-for score, chunk in top_3:
-    context+= chunk+ "\n\n"
-
-print("\n----CONTEXT----\n")
-print(context[:1500])
+    
 
 
-#######################################################3
-#connecting to LLM
 
-#load token
 
-client = InferenceClient(
-    api_key = os.getenv("HF_TOKEN")
-)
 
-prompt = f"""
-Answer the question using ONLY the provided context.
 
-Context: 
-{context}
 
-Question:
-{query}
 
-"""
 
-##############################################################3
-#generating answer
 
-response = client.chat_completion(
-    model="meta-llama/Llama-3.1-8B-Instruct",
-    messages=[
-        {
-            "role":"user",
-            "content":prompt
-        }
-    ],
-
-    max_tokens=300
-)
-
-print("\n====ANSWER====\n")
-print(response.choices[0].message.content)
